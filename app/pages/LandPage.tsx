b80852d9954a6db0e030f161b57d4f5e62214a30
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2025-08-08 15:31:29 -0400

    Add landing page content

--- app/pages/LandPage.tsx
+++ app/pages/LandPage.tsx
@@ -1,5 +1,29 @@
 export default function LandingPage() {
-    return <>
-        <h1>Blacksheep</h1>
-    </>
+    return <main id='landing-page-container'>
+        <h1>We all need supportive community.<br />
+            If you live in the Mississauga area, get connected today!</h1>
+        <p>LEARN MORE!</p>
+        <div id='videos-section-container'>
+            <div id='videos-items-container'>
+                <div className="video">
+                    Video 1
+                </div>
+                <div className="video">
+                    Video 2
+                </div>
+            </div>
+            <h2>COMING SOON</h2>
+        </div>
+        <div id='blog-section-container'>
+            <h2>WHY DOES COMMUNITY MATTER?</h2>
+            <p>Increase your knowledge on the benefits of spirtiual commnity by following our monthyly blog.</p>
+            <div id='blog-items-container'>
+                <div className="blog-item">
+                    <h3>PLURALITY IN BIBLICAL ONENESS</h3>
+                </div>
+                <div className="blog-item"><h3>THE ART OF STORYTELLING</h3></div>
+                <div className="blog-item"><h3>A COMMENTARY ON MERCY</h3></div>
+            </div>
+        </div>
+    </main>
 }
