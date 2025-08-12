64bfd09876c957244c5f507870f2b01d2e2ca957
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2025-08-12 14:14:24 -0400

    Add new video

--- app/layouts/Footer.tsx
+++ app/layouts/Footer.tsx
@@ -4,11 +4,11 @@ export default function Footer() {
             <h2>LEGAL</h2>
             <p>TERMS & CONDITIONS</p>
             <p>PRIVACY POLICY</p>
-            <p>2025 BLACKSHEEP</p>
+            <p>&copy; 2025 BLACKSHEEP</p>
         </div>
         <div id='right'>
             <h2>CONTACT US</h2>
-            <div>SOCIAL MEDIA</div>
+            {/* <p>SOCIAL MEDIA</p> */}
             <p>contact.us.blacksheep@gmail.com</p>
         </div>
     </footer>
