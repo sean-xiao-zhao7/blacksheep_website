3ac5b3c1e841102ac803fc5953209712091a1396
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2025-08-13 13:42:13 -0400

    Add all base pages

---
+++ app/pages/legal/TermsPage.tsx
@@ -0,0 +1,16 @@
+export default function TermsPage() {
+    return <div className='text-page-container'>
+        <h1>Art Of Story Telling</h1>
+        <div>
+            <p>We all need supportive community.</p>
+            <p>Call, text, or meet with a Christian faith-based community leader in the Mississauga area today.</p>
+            <p>We know it isn't easy meeting people, and the alternative poses just as many problems. Isolation has never helped anyone. Yet, there are tons of communities out there just ready and willing to welcome us, and so many people in search of just that!</p>
+            <p>Blacksheep is a way to connect with them.</p>
+            <p>We gather the bare minimum of information about you (not even a profile picture!) and connect you to community groups, not based on preference, culture, or any other variable other than 'they're literally right next to you!'</p>
+            <p>Only after connecting with the first community, if you find that they're not really your cup of tea, only then can you dismiss that community from your search and then the system match you with the next nearest community near you.</p>
+            <p>Instead of a 'swipe left' culture where people give up on you without ever having the chance to actually meet you, this changes the entire game!</p>
+            <p>But we're not a dating app, we're simply giving you an introduction to new people. The possibilities of what could happen</p>
+            <p>Coming Soon!</p>
+        </div>
+    </div>
+}
