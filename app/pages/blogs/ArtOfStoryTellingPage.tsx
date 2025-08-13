13090d3566785174fda1c2e4936d7c9377ca89a7
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2025-08-13 14:37:36 -0400

    Complete site

--- app/pages/blogs/ArtOfStoryTellingPage.tsx
+++ app/pages/blogs/ArtOfStoryTellingPage.tsx
@@ -5,15 +5,196 @@ export default function ArtOfStoryTellingPage() {
             <img src='/public/assets/images/the_art_of_storytelling2.png' alt='oneness' />
         </div>
         <div>
-            <p>We all need supportive community.</p>
-            <p>Call, text, or meet with a Christian faith-based community leader in the Mississauga area today.</p>
-            <p>We know it isn't easy meeting people, and the alternative poses just as many problems. Isolation has never helped anyone. Yet, there are tons of communities out there just ready and willing to welcome us, and so many people in search of just that!</p>
-            <p>Blacksheep is a way to connect with them.</p>
-            <p>We gather the bare minimum of information about you (not even a profile picture!) and connect you to community groups, not based on preference, culture, or any other variable other than 'they're literally right next to you!'</p>
-            <p>Only after connecting with the first community, if you find that they're not really your cup of tea, only then can you dismiss that community from your search and then the system match you with the next nearest community near you.</p>
-            <p>Instead of a 'swipe left' culture where people give up on you without ever having the chance to actually meet you, this changes the entire game!</p>
-            <p>But we're not a dating app, we're simply giving you an introduction to new people. The possibilities of what could happen</p>
-            <p>Coming Soon!</p>
+            <p>To every story, there are five main structural features that every reader is guided through so that when you arrive at the end of the story – if the story is told well – it leaves you with an understanding of all that it carefully and masterfully has woven together as it directs you through the journey.
+            </p>
+            <p>Those five elements are listed here:</p>
+            <p>- Exposition</p>
+            <p>- Rising Action</p>
+            <p>- Climax</p>
+            <p>- Falling Action</p>
+            <p>- Resolution (or Denouement)</p>
+            <p>Unfortunately, when many are handed a Bible, it’s the one book in the world where the rules for reading a story seem to get disregarded in the most peculiar of ways.
+            </p>
+            <p>Some read it like a “magic” book, opening to pages at random and hoping to hear God’s manifold wisdom speak to them in their decision-making for life choices, kind of like a magic eight ball.
+
+                Others choose to focus in on only one verse, one chapter, and even have ‘favourites’ that they stick to reading repetitively throughout their entire faith journey.
+
+                But imagine if you were to read any other book in this way, just how lost and confused you would be even though you might go through life feeling quite confident that you understood or could even explain the story to others?
+
+                I just recently watched the new John Wick movie in theatres. For those who are aware of it, you probably already know that this is the fourth chapter in the story just recently released. In planning a group trip to see the movie this week, one of my friends was unable to make it out with us. When I later spoke with him the next day, he mentioned that Chapter Four was the first part he was going to see.
+            </p><p>
+                I immediately stopped him and told him that if he hadn’t seen the first three chapters, although he might get a thrill from the action scenes and find entertainment in the movie in general, he would be unable to understand the greater theme of all that was taking place, and most importantly, why.
+
+                This is the same reason why reading the Bible in parcelled out portions leaves so many to feel disconnected.
+
+                They read one book of the Bible where they see the mention of Jesus healing and the authority He gives to those who follow Him to also perform the same in His Name, but then they miss some crucial information in another book where the church elders give an almost legal fine print parentheses, mentioning that, “if you fail to honor your wife, your prayers would be hindered.”
+
+                And that’s just one of hundreds of ‘fine print’ clauses to understanding, explaining, and then living out the liberty we have in Christ in a functional way in this world.</p>
+            <p>
+                So, what is the story told in Scripture?
+
+            </p>
+            <p>EXPOSITION:</p>
+            <p>The opening five books of the Bible, often referred to as the Torah or, “The Law,” are where some of the most foundational elements of the overall story are revealed.
+                It’s here that we learn:
+            </p>
+            <p>- That God created everything
+                <br />- That God is a person and can be communicated with as such
+                <br />- That God created us in His image (though we’re not quite told what that even means just yet)
+                <br />- God’s intention for humanity
+                <br />- The liberty that Freewill grants us, and the pivotal role it plays in godly love
+                <br />- The detriment of self-government
+                <br />- The fact that we have an enemy who craftily leads us astray from God’s intention
+                <br />- God’s promise
+                <br />- The repercussions of sin on a fallen humanity
+                <br />- God’s solution to our self-inflicted problem
+                <br />- God’s sovereignty over human affairs and foreknowledge of our outcomes even before we know what to do with it all.
+            </p>
+            <p>And most of that is uncovered in just the first three chapters of Genesis! (Whereas the Torah consists of the entirety of the first five books of the Bible: Genesis, Exodus, Leviticus, Numbers, & Deuteronomy)
+
+
+                A part of the Torah is also the commandments. Most are familiar with these, but even the Commandments disconnected from the whole story leads many to live out a very confused profession of faith.
+
+            </p>
+            <p>RISING ACTION</p>
+            <p>
+                Fast forward to the next historical “chapter” in humanity’s story, and we begin to behold the children of Israel, equipped with God’s law now entering into the Promised Land and what ought to have been the “glory days” of God’s chosen people, right?
+
+            </p>
+            <p>Wrong.</p>
+            <p>We would slowly but gradually learn over a timeline spanning more than 1000 years (and still today unfortunately) that whether through prophet, priest, or king, humanity seems to continue to demonstrate every despicable act known to mankind right from chapter four of Genesis onward:
+            </p>
+            <p>Murder
+                <br />
+                Greed
+                <br />
+                Lust
+                <br />
+                Envy
+                <br />
+                Pride
+                <br />
+                Arrogance
+                <br />
+                Sexual immorality
+                <br />
+                Hatred
+                <br />
+                Superiority
+                <br />
+                Narcissism
+                <br />
+                Selfishness
+                <br />
+                Bigotry…
+            </p>
+            <p>
+                The list goes on considerably.
+
+                But all the while, a continuing promise weaves in and out of every book, providing some silver lining of hope that something better is coming for those who hold fast to His promise.
+
+            </p>
+            <p>CLIMAX</p>
+            <p>Then, two thousand years ago, something out of the ordinary happens… A child is born.
+
+                His origins: supernatural intertwined with the natural… A virgin gives birth.
+            </p><p>
+                And indeed, this is no ordinary child. For it’s been said of Him that if all the stories were ever written down regarding everything He said and did, there would never be enough books in the world to contain them… a serious admonition to those bent on regurgitating inane Christian clichés.
+
+                This child grows in the natural course of things to a man, who at the age of thirty begins attending to what He dubbed, “His Father’s business” since childhood, but now with fierce veracity.
+
+                The miracles can’t be understated, even if we’ve grown so accustomed to hearing about them that they’ve somehow lost their ability to excite the imagination anymore.
+
+                – He healed a man blind from birth and caused him to see…
+
+                Before I begin listing the rest and become one guilty of my own lack of awe, let me say that again in another way.
+
+                Your friend has a child. That child is born blind. Everyone in your family and community knows or has heard of this. Some have even met him. You grew up watching him stumble as a child; trip and lean on others as a teenager; and then led by a white cain as an adult. Watching him feel around for his way through life has become somewhat commonplace to you.
+
+                Day after day, week after week, year after year, and then decade after decade, you witnessed this, then somehow, as his path intersects with the One they call Jesus. He arrives home – this man you’ve grown accustomed, even familiar with, arrives home with His full sight restored nearing forty years old!</p>
+            <p>What would you feel?
+                What would you think?
+                How would that challenge what you always believed to be true?
+
+                Think about this carefully so as to not gloss over these next points too expediently:</p>
+            <p>
+                <ul>
+                    <li>He turned water into wine</li>
+                    <li>With a word he healed a soldiers servant over 50 kms away</li>
+                    <li>He drove seven demonic spirits out of a woman</li>
+                    <li>He drove thousands of demonic spirits out of one man and restored what we would call today a lunatic, back to a sane functioning life</li>
+                    <li>He raised a man from the dead, even after he was buried in a tomb for days</li>
+                    <li>He walked on water</li>
+                    <li>He healed people with incurable diseases by simply touching them</li>
+                    <li>He told a storm to stop – and it stopped!</li>
+                    <li>He caused a paraplegic to find functionality in his arms and legs</li>
+                    <li>He caused a man born with a stub for an arm to grow a fully functioning arm.</li>
+                </ul>
+            </p>
+            <p>And, in an almost anticlimactic act of someone with this resume of supernatural wonder, he allows Himself to be taken, beaten, tortured, and killed… on a cross.
+            </p>
+            <p>FALLING ACTION </p>
+            <p>His followers? Fled.
+
+                His life story, probably soon to become myth for those a generation or two (muchless two millenia) separated from these historical events.
+
+                His cause? Misunderstood at best. Downright disbelieved, ridiculed, and insulted at worst.
+
+                The vast majority return to ‘life as normal’ and even his followers are uncertain what to make of it all.
+
+                Dinner parties are held, birthdays celebrated, ambitions pursued, possessions acquired, all with little or no thought of what it all meant, or what it was all for, or what it all accomplished.
+
+                Conjectures form as they always do.
+
+                But those speculations are very short lived… Three days later.</p>
+            <p>RESOLUTION</p>
+            <p>Jesus is seen alive once again! Walking, talking, and live in the flesh!
+
+                His tomb where he was buried: empty.
+
+                His body? Never recovered even until today. (How could it?)
+
+                The Holy Spirit of God begins to pour out over the world as we know it, starting with His followers.
+
+                Most people easily seem to understand the concept that a demonic spirit can make a person do evil things…
+
+                Few, however, seem to understand what the Holy Spirit of God in a person can do or does!
+
+                God begins sharing a revelation of all that His death accomplishes by giving His Holy Spirit to the early believers.
+
+                All of a sudden, believers are performing the very miracles He once did!
+
+                A man who couldn’t walk: walks! But not by the direct touch of Jesus – instead by the touch of one of His followers.
+
+                A girl with a demonic spirit finds freedom and deliverance through another disciple from an evil spirit, and her inner tortures cease.
+
+                A misguided understanding of all the Holy books that make up the Old Testament today, which were the same books relied upon for so long by religious leaders everywhere, but who never understood what it pointed to in Jesus as the Messiah are finally starting to be understood…</p>
+            <p>It’s summary?</p>
+            <p>Jesus came to this earth to fulfill His promise to Adam – the first man – that had been carefully and purposefully concealed right up to the divinely timed moment when He would fulfill it all through His life, death, and resurrection!
+
+                He begins to show believers Himself in every book of the Bible starting from Genesis:
+            </p>
+            <p>
+                - the coats of fur that covered man’s shame in the Garden
+                <br />- the son who was about to be offered up by Abraham on the mountain, but stopped at the last moment by an angel
+                <br />- the blood that was used to cover the doorposts so that the Angel of Death would “pass over” their homes
+                <br />- the bronze snake that Moses lifted up in the wilderness
+                <br />- the reason for the preservation of the lineage of the Israelite from which the “Lion of the Tribe of Judah” would descend
+                <br />- the preservation of the Scriptures that work as a mirror as they reflect the awful truth of our own sin infected condition and their subsequent shortcomings in us…
+
+            </p>
+            <p>And for the “coup de gras”, the promised Messiah revealed in all His glory – yes, indeed as a conquering King – but not the kind of our own imaginations, but rather of the order of a suffering servant sent by God to set all of those who believe, free!
+
+                Sixty-six books, all carefully arranged. Each one building chapter upon chapter, verse upon verse, to the great hope in all that we’ve ever truly longed to know… about our Creator… about ‘us’ as a creation… about His great purpose and intention for our lives.
+
+                But most of all… of the heights, and width, and breadth of a love far too profound to be told about in one sitting…
+
+                In fact, even in one lifetime!
+            </p>
+            <p>So, although there might be many reasons culturally, ideologically, and even historically for celebrating to goodness of Good Friday each year, if we fail to comprehend the “full story” for the immeasurable gift given to humanity on this day and how He has the power not only to restore a right relationship between man and God, but also between mankind, one to another, then we have also failed to comprehend love – the most truest essence of His divinely orchestrated expression of love – and the fullness of what it truly means.
+
+                I pray that you find meaningful connection to others who are willing to walk with you in this journey.
+            </p>
         </div>
     </div>
 }
