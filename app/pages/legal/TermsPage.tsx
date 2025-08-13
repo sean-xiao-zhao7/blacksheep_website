37213ffb6b7c80aedee6b87ae26cc0fd5cd753d0
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2025-08-13 13:50:29 -0400

    Add routes

--- app/pages/legal/TermsPage.tsx
+++ app/pages/legal/TermsPage.tsx
@@ -1,16 +1,88 @@
 export default function TermsPage() {
     return <div className='text-page-container'>
-        <h1>Art Of Story Telling</h1>
+        <h1>Terms & Conditions</h1>
         <div>
-            <p>We all need supportive community.</p>
-            <p>Call, text, or meet with a Christian faith-based community leader in the Mississauga area today.</p>
-            <p>We know it isn't easy meeting people, and the alternative poses just as many problems. Isolation has never helped anyone. Yet, there are tons of communities out there just ready and willing to welcome us, and so many people in search of just that!</p>
-            <p>Blacksheep is a way to connect with them.</p>
-            <p>We gather the bare minimum of information about you (not even a profile picture!) and connect you to community groups, not based on preference, culture, or any other variable other than 'they're literally right next to you!'</p>
-            <p>Only after connecting with the first community, if you find that they're not really your cup of tea, only then can you dismiss that community from your search and then the system match you with the next nearest community near you.</p>
-            <p>Instead of a 'swipe left' culture where people give up on you without ever having the chance to actually meet you, this changes the entire game!</p>
-            <p>But we're not a dating app, we're simply giving you an introduction to new people. The possibilities of what could happen</p>
-            <p>Coming Soon!</p>
+            <p>The following terms of use agreement sets out your rights and obligations in respect of your use of the BlackSheep website and certain access controlled portions of the website and/or app, the "members-only area".
+            </p>
+            <p>Acceptance</p>
+            <p>It is important that you read the following terms and conditions carefully. This terms of use agreement ("Agreement") is a legal agreement between you and BlackSheep, the owner and operator of this website and/or app, including, without limitation, the members-only area (the "Website" and/or “App”). It states the terms and conditions under which you may access and use the Website and/or App and all written and other materials displayed or made available through the Website and/or App, including, without limitation, text, and software (collectively, the "Content"). By accessing and using the Website and/or App, you are indicating your acceptance to be bound by the terms and conditions of this Agreement.
+            </p>
+            <p>If you do not accept these terms and conditions, you must not access or use the Website and/or App. BlackSheep may revise this Agreement at any time by updating this posting. Use of the Website and/or App after such changes are posted will signify your acceptance of these revised terms. You should visit this page periodically to review this Agreement.
+            </p>
+            <p>You agree that electronic pages of the Website and/or App are the equivalent of written documents and that your signifying agreement or acknowledgement by "clicking" any buttons, stating "agree", (or its equivalent) or by typing "agree" (or its equivalent) where prompted by the Website and/or App shall have the same legal effect as if you had placed your signature by hand and provided hard-copy versions of such pages to BlackSheep with the intention to be legally bound.
+            </p>
+            <p>Use of the Website and/or App
+                Note: The usage of the terms ‘Website and/or App’ include community connections made through the Website and/or App.</p>
+            <p>Subject to your acceptance to be bound by any additional or other terms and conditions applicable to all or part of the BlackSheep website and/or App, BlackSheep authorizes you to access and use the Website and/or App for your personal non-commercial use in Canada in accordance with the terms and conditions of this Agreement. The information on the Website and/or App is for informational purposes only; it is not intended as an interactive forum and it is not professional legal or medico-legal advice nor is it intended to be a substitute therefore. You should not use any information contained on the Website and/or App as a substitute for consultation with legal professionals or other professional advisors. You should not act or abstain from acting based upon information obtained from the Website and/or App without first consulting appropriate professional advisors.
+            </p>
+            <p>The Website and/or App contains learning resources of a general nature that are presented for educational purposes for use by healthcare professionals. The information contained within such learning materials is for informational purposes only; it is not professional legal or medico-legal advice, nor is it intended to be a substitute therefore. Ideas and approaches contained within such learning materials DO NOT represent "the standard of care" for Canadian healthcare professionals. Standards of medical care are specific to the facts and circumstances of an individual case, and may be subject to change as scientific knowledge and technology advance and as practice patterns evolve.
+            </p>
+            <p>Membership</p>
+            <p>If you are a BlackSheep member (“Member”), you will create a username to create a password that will give you access to the members-only area. Your username and password are for your personal use only. You are responsible for maintaining the confidentiality of your username and password and are responsible for all activities that occur under your username and password. In the event that you forget your password, a password reset will be issued upon request. An automatic email will be sent to the primary email address stored on file.
+            </p>
+            <p>BlackSheep is under no obligation to confirm the actual identity or authority of any party accessing or using the Website and/or App using the members-only area under your username and password. You agree to notify BlackSheep immediately of any unauthorized use of your username or password or any other breach of security. BlackSheep will not be liable for any loss or damage arising from your failure to protect your username or your password.
+            </p>
+            <p>First-time users will be asked to create a new password right away. You agree that you will comply with the password selection guidelines as may be issued from time to time by the BlackSheep and that you shall change your password as prompted by the Website and/or App to reduce the potential for unauthorized use.
+            </p>
+            <p>Users who forget their password can reset it through the “Click here to Reset Password” link on the sign-in page. They will be prompted to enter their email on file and will receive a password reset link via email. </p>
+            <p>Alternatively, they can email contact.us.blacksheep@gmail.com and request a password reset from the system administrators.</p>
+            <p>You agree that, by accessing the members-only area using your username and password, BlackSheep may use your username and password as the means to authenticate your identity for the purposes of using the members-only area.
+            </p>
+            <p>Members-only area (Member sign-in)
+            </p>
+            <p>If you are a Member, BlackSheep authorizes you to access and use the members-only (member sign-in) area in relation to connecting with community in accordance with the terms and conditions of this Agreement. Subject to the terms and conditions of this Agreement, you are hereby granted a personal limited, non-transferable and non-exclusive license to access, view and use the members-only area and the community connection. You must also abide by any additional requirements governing privacy of personal information surrounding the use of any connections that are made in the members-only area.
+            </p>
+            <p>This information contained in the members-only area is provided for the purposes of supporting greater community connection. It is not intended to be a substitute for direct consultation with the BlackSheep, legal professionals or other professional advisors with regard to specific spiritual or medical issues.
+            </p>
+            <p>Communications (Messenger)</p>
+            <p>BlackSheep has provided a secure messaging service for Members seeking to communicate with community leaders. You agree that you will contact the community members to obtain advice only by using the secure messaging service available from within the members-only area. BlackSheep mentors may not respond to unencrypted e-mail from Members seeking advice or assistance. BlackSheep cannot guarantee the confidentiality of such unencrypted e-mail transmissions.
+            </p>
+            <p>No special relationship or obligation will result from the transmission of e-mail or electronic messaging generated outside the members-only area from Members to the BlackSheep for the purpose of seeking advice or assistance. Such communications will not be responded to and will be discarded unread.
+            </p>
+            <p>BlackSheep has made reasonable efforts to ensure the security of communication between you and BlackSheep App using the secure messaging service. However, as with any electronic communication, BlackSheep cannot and does not represent, warrant or covenant that communications to or from the members-only area using the secure messaging service will under all circumstances be secure, not intercepted or subject to delay, data corruption or unauthorized access by current or future means.
+            </p>
+            <p>If you wish to transmit sensitive information, documents or other materials through the BlackSheep App electronically, this should be done via the secure messaging service, and solely at your own risk. This service allows you to transmit such materials to the BlackSheep members for the purposes of communicating questions or seeking advise. You agree that you will not upload or transfer any files, documents or materials: (i) which are unlawful, abusive, defamatory or obscene including without limitation any such material constituting or encouraging conduct that would constitute a criminal offence, create civil liability or otherwise violate any local provincial, national or international law or regulation, or (ii) which knowingly contains a virus or other harmful component(s).
+            </p>
+            <p>You also agree that copies of all communication between you and community members using BlackSheep’s secure messaging service and copies of the files, documents and other materials that you forward using the BlackSheep App through the members-only area of the BlackSheep Website and/or App become the property of BlackSheep.
+            </p>
+            <p>
+                Limitation of liability
+            </p>
+            <p>WHILE BLACKSHEEP ENDEAVORS TO PROVIDE ACCURATE AND CURRENT INFORMATION AS WELL AS SAFE COMMUNITY CONNECTIONS ON ITS WEBSITE AND/OR APP, IN NO EVENT SHALL BLACKSHEEP, ITS STAFF, COMMUNITY MEMBERS, VOLUNTEERS, AGENTS, LICENSORS, SUPPLIERS AND THEIR RESPECTIVE SUCCESSORS AND ASSIGNS BE LIABLE FOR ANY LOSSES, EXPENSES, DAMAGES OR ANY OTHER AMOUNTS OF ANY KIND, INCLUDING, WITHOUT LIMITATION, ANY DIRECT, SPECIAL, INDIRECT, PUNITIVE, INCIDENTAL OR CONSEQUENTIAL DAMAGES WHETHER OR NOT INCURRED IN CONNECTION WITH YOUR USE, MISUSE OR RELIANCE UPON THE WEBSITE AND/OR APP OR CONTENT, OR YOUR INABILITY TO USE THE WEBSITE AND/OR APP, REGARDLESS OF THE CAUSE AND WHETHER ARISING IN CONTRACT (INCLUDING FUNDAMENTAL BREACH), TORT (INCLUDING NEGLIGENCE), OR OTHERWISE. THE FOREGOING LIMITATION SHALL APPLY EVEN IF BLACKSHEEP KNEW OF OR OUGHT TO HAVE KNOWN OF THE POSSIBILITY OF SUCH DAMAGES.
+            </p>
+            <p>YOU ACKNOWLEDGE AND AGREE THAT YOUR ACCESS TO AND USE OF THE WEBSITE AND THE CONTENT IS ENTIRELY AT YOUR OWN RISK AND LIABILITY.
+            </p>
+            <p>Modification to the Website</p>
+            <p>BlackSheep reserves the right at any time and from time to time to modify or discontinue, temporarily or permanently, the Website and/or App (or any part thereof) with or without written notice. You agree that BlackSheep shall not be liable to you or any third party for any modifications, suspension or discontinuance of the Website or any part thereof.
+            </p>
+            <p>Downtime</p>
+            <p>You acknowledge that the Website and/or App and any part thereof may be periodically unavailable in order to allow for maintenance.
+            </p>
+            <p>Unauthorized use
+            </p>
+            <p>Unauthorized attempts to defeat or circumvent security features, to use the BlackSheep App or system for other than intended purposes, to deny service to authorized users, to access, obtain, alter, damage, or destroy information, or otherwise to interfere with the system or its operation are not permitted. Evidence of such acts may also be disclosed to law enforcement authorities and result in criminal prosecution under the laws of Canada or such other jurisdictions as may apply.
+            </p>
+            <p>Waiver</p>
+            <p>Any consent by BlackSheep to, or waiver of, a breach of this Agreement which you have committed, whether express or implied, shall not constitute a consent to, or waiver of, any other, different or subsequent breach.
+            </p>
+            <p>Severability</p>
+            <p>The invalidity or unenforceability of any provision of this Agreement or any covenant contained herein shall not affect the validity or enforceability of any other provision or covenant contained herein and any such invalid provision or covenant shall be deemed severable from the rest of this Agreement.
+            </p>
+            <p>Termination</p>
+            <p>BlackSheep may, in its sole discretion, terminate or suspend your right to use the Website and or App, or any part of the Website and/or App, at any time without notice. In the event of such termination, or suspension, you are no longer authorized to access, view or use the Website and/or App, or the part of the Website and/or App affected by such termination or suspension. The restrictions imposed on you with respect to material downloaded from the Website and/or App, the disclaimers and the limitation of liability set forth in this Agreement, shall survive termination or suspension of this Agreement for any cause. BlackSheep shall not be liable to you or any other party for such termination or suspension.
+            </p>
+            <p>Privacy</p>
+            <p>We understand the importance of privacy and the protection of personal information. Read the entire BlackSheep Privacy Policy to learn how we collect, use, and disclose personal information.
+            </p>
+            <p>Governing law and jurisdiction
+            </p>
+            <p>The Website is operated by BlackSheep Administrators within the province of Ontario, Canada. You agree that all matters relating to your access, or use of the Website and/or App and its Content shall be governed by the laws of the province of Ontario and the laws of Canada applicable therein, without regard to conflict of laws principles. You agree and hereby submit to the exclusive and preferential jurisdiction of the courts of the province of Ontario with respect to all matters relating to your access and use of the Website and the Content as well as any dispute that may arise therefrom.
+            </p>
+            <p>Notice</p>
+            <p>If you have general questions or comments regarding the BlackSheep Website and/or App contact us:
+            </p>
+            <p>By email</p>
+            <p>contact.us.blacksheep@gmail.com</p>
         </div>
     </div>
 }
--- app/routes.ts
+++ app/routes.ts
@@ -2,5 +2,13 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     route('/learn-more', "./pages/LearnMorePage.tsx"),
+
+    route('/blogs/plurality', "./pages/blogs/PluralityInBiblicalOnenessPage.tsx"),
+    route('/blogs/storytelling', "./pages/blogs/ArtOfStoryTellingPage.tsx"),
+    route('/blogs/mercy', "./pages/blogs/ACommentaryOnMercyPage.tsx"),
+
+    route('/terms', "./pages/legal/TermsPage.tsx"),
+    route('/privacy', "./pages/legal/PrivacyPage.tsx"),
+
     index("routes/home.tsx"),
 ] satisfies RouteConfig;
